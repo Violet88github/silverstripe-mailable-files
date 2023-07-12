db7ba7b78de7688cbe3db1290b57cd2ffb5fed4b
PixNyb <contact@roelc.me>
2023-07-12 11:40:38 +0200

    Added mail from field

--- src/DataExtensions/MailableFilesPageExtension.php
+++ src/DataExtensions/MailableFilesPageExtension.php
@@ -3,18 +3,24 @@
 namespace Violet88\MailableFilesModule\DataExtensions;
 
 use SilverStripe\Forms\CheckboxSetField;
+use SilverStripe\Forms\EmailField;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\Form;
 use SilverStripe\Forms\FormAction;
 use SilverStripe\Forms\GridField\GridField;
 use SilverStripe\Forms\GridField\GridFieldConfig;
 use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
+use SilverStripe\Forms\TextField;
 use SilverStripe\ORM\DataExtension;
 use UndefinedOffset\SortableGridField\Forms\GridFieldSortableRows;
 use Violet88\MailableFilesModule\DataObjects\MailableFile;
 
 class MailableFilesPageExtension extends DataExtension
 {
+    private static $db = [
+        'MailFrom' => 'Varchar(255)',
+    ];
+
     private static $many_many = [
         'MailableFiles' => MailableFile::class
     ];
@@ -25,11 +31,15 @@ class MailableFilesPageExtension extends DataExtension
 
     public function updateCMSFields(FieldList $fields)
     {
-        $fields->addFieldToTab('Root.MailableFiles', GridField::create(
-            'MailableFiles',
-            'Mailable Files',
-            $this->owner->MailableFiles(),
-            GridFieldConfig_RecordEditor::create()
-        ));
+        $fields->addFieldsToTab('Root.MailableFiles', [
+            EmailField::create('MailFrom', 'Mail from address')
+                ->setDescription('This is the email address that will be used as the "from" address when sending emails.'),
+            GridField::create(
+                'MailableFiles',
+                'Mailable Files',
+                $this->owner->MailableFiles(),
+                GridFieldConfig_RecordEditor::create()
+            )
+        ]);
     }
 }
